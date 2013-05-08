fd8e056982d1ae85c59cd73a5953b953c35d0341
kenns <kennsmacintosh@gmail.com>
2013-05-07 21:21:01 -0700

    gui

--- gui.rb
+++ gui.rb
@@ -7,9 +7,16 @@ Shoes.app(:width => 500,:height => 700 ) do
   # port = serv_addr.split(':')[1]
   # serv = TCPSocket.open(hostname,port)
   stacks do
-    edit_box
-    edit_box :width => 460, :height => 400
-    # flows do
+	para strong("") "Please enter the server address"
+	flow do
+		@server_name = edit_box
+		edit_box :width => 460, :height => 400
+		@connect = button "Connect"
+	end
+	@connect.click{
+		
+	}
+	# flows do
     #   edit_box
     #   edit_box :width => 400, :height => 250
     #   button "send"
