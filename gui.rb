12a68e07d3cac7c3fa7c2fabc2de0606827cfd6d
kenns <kennsmacintosh@gmail.com>
2013-05-09 14:45:19 -0700

    a

--- gui.rb
+++ gui.rb
@@ -1,32 +1,26 @@
 require 'socket'
-Shoes.app(:width => 500,:height => 700 ) do
+Shoes.app(:width => 500,:height => 500 ) do
   # begin
   #   serv_addr = ask("Please, enter server address:") #ask for server address
   # end while not serv_addr =~ /(.+):(\d+)/
   # hostname = serv_addr.split(':')[0]
   # port = serv_addr.split(':')[1]
   # serv = TCPSocket.open(hostname,port)
-  stacks do
-	para strong("") "Please enter the server address"
-	flow do
-		@entered_text = edit_box :width => 460, :height => 400
-		@connect = button "Connect"
-	end
-	@connect.click{
-		serv_name = @entered_text.text
-		if(not serv_name =~ /(.+):(\d+)/)
-			#clear text box and empty entered_text
-		else
-			hostname = serv_name.split(:)[0]
-			port = serv_name.split(:)[1]
-			client = ChatClient.new (hostname, port)
-		end
-	}
 	# flows do
     #   edit_box
     #   edit_box :width => 400, :height => 250
     #   button "send"
     # end
-  end
-  
+	background "#EFC"
+	flow :width => 500, :margin => 10 do
+		stack :width => "50%" do
+			@names = edit_box :height => "300"
+		end
+		stack :width => "50%" do
+			@chat = edit_box :height => "300"
+		end
+		stack :width => "100%" do
+			@msg = edit_box
+		end
+	end
 end
